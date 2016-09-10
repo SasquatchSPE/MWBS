18c3d46496b84759741f5ac55cd62e6e6c15088d
SasquatchSPE <tomekmakow@gmail.com>
2016-09-10 00:23:12 -0400

    Added UI wireframe

---
+++ MWBS/Models/PuzzleSolutionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MWBS.Models
+{
+    public class PuzzleSolutionModel
+    {
+        public List<List<string>> Words { get; set; }
+        public List<string> SolutionList => Words.Select(w => string.Join(",  ", w)).ToList();
+    }
+}
